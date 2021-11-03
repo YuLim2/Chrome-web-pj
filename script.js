c7af3a336edeb9a38f7df26e6198fac27317bac2
YuLim2 <jyulim0120@gmail.com>
2021-11-03 22:36:48 +0900

    선언, 함수 만들기

---
+++ script.js
@@ -0,0 +1,9 @@
+const loginFrom = document.getElementById("login-form");
+const loginInput = loginFrom.querySelector("input");
+const loginButton = loginFrom.querySelector("button");
+
+function onLoginBtnClick() {
+
+}
+
+loginButton.addEventListener("click", onLoginBtnClick);
