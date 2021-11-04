2d1cf201e3777492247f4a593b7000429be1b0d8
YuLim2 <jyulim0120@gmail.com>
2021-11-04 18:40:43 +0900

    submit 누르면 클래스 교환

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-const loginFrom = document.getElementById("login-form");
-const loginInput = loginFrom.querySelector("input");
-const loginButton = loginFrom.querySelector("button");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
 
-function onLoginBtnClick() {
 
+const HIDDEN_CLASSNAME = "hidden";
+const pass = "kingyulim";
+
+function onLoginSubmit(event) {
+    event.preventDefault(); //기본동작 실행 X
+    loginForm.classList.add(HIDDEN_CLASSNAME); //로그인 폼 숨기기
+    const username = loginInput.value;
+    if (username === pass) { //내가 로그인 한다면
+        console.log(username);
+        greeting.innerText = `Hello ${username}`;
+        greeting.classList.remove(HIDDEN_CLASSNAME);
+    } else { //이외의 사람이 로그인 한다면
+        greeting.innerText = `누구...?`;
+        greeting.classList.remove(HIDDEN_CLASSNAME);
+    }
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+
+loginForm.addEventListener("submit", onLoginSubmit);
