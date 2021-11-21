84ebf70207f7034466d3c5338e2fbfb094c16ca1
YuLim2 <jyulim0120@gmail.com>
2021-11-21 23:44:36 +0900

    오류 수정, 받은 데이터 로컬스토리지에 저장하고 리로드해도 로컬스토리지에서 불러오기

--- JS/todo.js
+++ JS/todo.js
@@ -1,8 +1,11 @@
-const toDoInput = toDoForm.querySelector("input"); //toDo 텍스트 적는 곳
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("#todo-form input"); //toDo 텍스트 적는 곳
 const toDoList = document.getElementById("todo-list"); //toDo 리스트
 
+
 const TODOS_KEY = "todos";
-const toDos = [];
+
+let toDos = [];
 
 function saveToDos() {
     localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
@@ -10,17 +13,18 @@ function saveToDos() {
 
 function deleteToDo(event) {
     const li = event.target.parentElement; //event가 일어난 element의 parentElement 선택
+    console.log(li.id);
     li.remove();
 }
 
 function paintToDo(newTodo) { //handleToDoSubmit에서 newTodo를 받아서 리스트에 추가
     const li = document.createElement("li");
+    li.id = newTodo.id;
     const span = document.createElement("span");
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     const button = document.createElement("button");
     button.innerText = "👍";
     button.addEventListener("click", deleteToDo);
-
     li.appendChild(span);
     li.appendChild(button);
     toDoList.appendChild(li);
@@ -30,20 +34,21 @@ function handleToDoSubmit(event) {
     event.preventDefault(); //초기 화면으로 돌아오는 걸 방지
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(newTodo);
-    paintToDo(newTodo);
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now(),
+    };
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
     saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
 
-// function sayHello(item) {
-//     console.log("this is the turn of", item);
-// }
+const savedToDos = localStorage.getItem(TODOS_KEY);
 
-const saveToDos = localStorage.getItem(TODOS_KEY);
-
-if (saveToDos !== null) {
-    const parsedToDos = JSON.parse(saveToDos); //array
-    parsedToDos.forEach((item) => console.log("this is the turn of", item)); //각각의 투두에 sayHello 부르기
+if (savedToDos !== null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo); //각각의 투두에 paintToDo 부르기
 }
